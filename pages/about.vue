a04c16d64a6d98a0ce932416b22cfc6aec3b8514
phrobinet <ph.robinet@gmail.com>
2022-08-24 17:24:14 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <div>
+    <h1>About page</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <div>
+    <h1>Index page</h1>
+    <nuxt-link to="/about">About page</nuxt-link>
+  </div>
+</template>
+
+<style scoped></style>

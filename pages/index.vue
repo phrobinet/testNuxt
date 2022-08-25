5955054a61143213e243abf7b30af6ec5e7cd917
phrobinet <ph.robinet@gmail.com>
2022-08-25 06:59:38 +0200

    update 9 buidlspec

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,17 @@ import { ref } from "vue";
   <div>
     <h1>Index page</h1>
     <nuxt-link to="/about">About page</nuxt-link>
+    <p>
+      Je reviens vers vous après notre entretien en date du 22 Juiller
+      concernant le recrutement du poste de développeur Full Stack, afin de
+      savoir où en est votre processus de sélection des candidats. Je vous
+      renouvelle ma profonde motivation pour ce poste et j’espère vous avoir
+      convaincu de la pertinence de ma candidature, ce poste étant parfaitement
+      en adéquation avec mes compétences. Je reste à votre disposition pour tout
+      renseignement complémentaire et je joins, à cet effet, mes coordonnées. Je
+      vous prie d’agréer, Madame/Monsieur [nom du recruteur], mes salutations
+      distinguées.
+    </p>
   </div>
 </template>
 
